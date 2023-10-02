88cedecf9a4232aad534122d4dfd76dbfccfeb3b
Amir55143 <aamirnoor55143@gmail.com>
2023-10-02 12:44:18 +0500

    Bloc

--- lib/bloc/theme/bloc/theme_bloc.dart
+++ lib/bloc/theme/bloc/theme_bloc.dart
@@ -1,13 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:theme_presistence/preferences/theme_preference.dart';
 
 part 'theme_event.dart';
 part 'theme_state.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
   ThemeBloc() : super(ThemeInitial()) {
-    on<ThemeEvent>((event, emit) {
-      // TODO: implement event handler
+    on<ChangeThemeEvents>((event, emit) {
+      emit(ThemeLoaded(themeData: appThemeData[event.name]!));
     });
   }
 }
