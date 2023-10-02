cadc1567ca9b524e32658942cae90696d2e126eb
Amir55143 <aamirnoor55143@gmail.com>
2023-10-02 12:38:12 +0500

    Event

--- lib/bloc/theme/bloc/theme_event.dart
+++ lib/bloc/theme/bloc/theme_event.dart
@@ -2,7 +2,13 @@ part of 'theme_bloc.dart';
 
 sealed class ThemeEvent extends Equatable {
   const ThemeEvent();
+  }
 
+  class ChangeThemeEvents extends ThemeEvent {
+    final String name;
+
+  const ChangeThemeEvents({required this.name});
   @override
-  List<Object> get props => [];
-}
+  List<Object?> get props => [name];
+
+  }
