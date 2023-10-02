420edac89ed1fcdc04f6fd4f8337ea65f0b978c0
Amir55143 <aamirnoor55143@gmail.com>
2023-10-02 13:03:36 +0500

    BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:theme_presistence/app/theme_app.dart';
+import 'package:theme_presistence/bloc/theme/bloc/theme_bloc.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Theme Preference',
-      home: ThemeApp(),
+    return  BlocProvider(
+      create: (context) => ThemeBloc(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Theme Preference',
+        home: ThemeApp(),
+      ),
     );
   }
 }
-
