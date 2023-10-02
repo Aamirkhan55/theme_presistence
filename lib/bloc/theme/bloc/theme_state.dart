f41150af49cb961762a46e3250d561e5a9b6d317
Amir55143 <aamirnoor55143@gmail.com>
2023-10-02 12:34:27 +0500

    State

--- lib/bloc/theme/bloc/theme_state.dart
+++ lib/bloc/theme/bloc/theme_state.dart
@@ -3,8 +3,19 @@ part of 'theme_bloc.dart';
 sealed class ThemeState extends Equatable {
   const ThemeState();
   
-  @override
-  List<Object> get props => [];
 }
 
-final class ThemeInitial extends ThemeState {}
+final class ThemeInitial extends ThemeState {
+  @override
+  
+  List<Object?> get props => [];
+}
+
+final class ThemeLoaded extends ThemeState {
+  final ThemeData themeData;
+
+  const ThemeLoaded({required this.themeData });
+  @override
+  List<Object?> get props => [themeData];
+
+}
